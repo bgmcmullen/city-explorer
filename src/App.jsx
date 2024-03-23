a3b1eb5ce3633c10c7ed71b4a347310a871f7fb4
Brendan <bgmcmullen@comcast.net>
2024-03-23 16:41:15 -0400

    added form

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputValue, setInputValue] = useState('');
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  }
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    alert(inputValue);
+  }
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Control placeholder="Enter location" onChange={handleChange}/>
+        </Form.Group>
+
+        <Button variant="primary" type="submit" onClick={handleClick}>
+          Explore!
+        </Button>
+      </Form>
     </>
   )
 }
